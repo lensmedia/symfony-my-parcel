19e134a5f832944c9bf73802b92723fe247537db
Maurice Andringa <72191119+MAndringa@users.noreply.github.com>
2026-08-19 16:09:58 +0200

    base file

---
+++ src/LensMyparcelBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lens\Bundle\MyparcelBundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class LensMyparcelBundle extends AbstractBundle
+{
+
+}
